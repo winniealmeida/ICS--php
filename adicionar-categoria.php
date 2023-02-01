32197dadb86e111650cdb6633691b7250a68af25
leticiamaiaa <79123176+leticiamaiaa@users.noreply.github.com>
2023-01-31 23:55:13 -0300

    Create adicionar-categoria.php

---
+++ adicionar-categoria.php
@@ -0,0 +1,11 @@
+h1>Adicionar categoria</h1>
+<form action="?page=salvar-categoria" method="POST">
+  <input type="hidden" name="acao" value="adicionar-categoria">
+	<div class="mb-3">
+    <label>Título</label>
+    <input type="text" name="nome_categoria" class="form-control">
+  </div><div class="mb-3">
+  <div class="mb-3">
+    <button class="btn btn-primary">Enviar</button>
+  </div>
+</form>
